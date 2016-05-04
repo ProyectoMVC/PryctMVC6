2bf5a7d70f6e5d73dac841b462ed0a4234c12599
ProyectoMVC <brunoe_1186@hotmail.com>
2016-05-03 23:01:17 -0500

    Diseño tienda virtual

---
+++ Final/src/Final.Entities/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final.Entities
+{
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public bool FlagAnulado { get; set; }
+    }
+}
---
+++ Final/src/Final.Entities/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final.Entities
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class Producto
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public bool FlagAnulado { get; set; }
+        public virtual Categoria IdCategoria { get; set; }
+    }
+}
