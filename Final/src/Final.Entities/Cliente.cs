a8c0b3e1781e2f8566452977263b49f0db2cfd87
ProyectoMVC <brunoe_1186@hotmail.com>
2016-05-08 18:17:14 -0500

    cliente

---
+++ Final/src/Final.Entities/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final.Entities
+{
+    public class Cliente 
+    {
+        public int IdCliente { get; set; }
+        public string NombreCliente { get; set; }
+        public int Apellido { get; set; }
+        public int Correo { get; set; }
+        public int Telefono { get; set; }
+        public string Direccion { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public bool FlagAnulado { get; set; }
+    }
+}
